e4f785185d49216066019c9f6f682bcc697240a0
VraiBrujah <mathieubrujah@gmail.com>
2025-07-27 16:20:14 -0400

    Update merci.php

--- merci.php
+++ merci.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Merci ! | Geek & Dragon</title>
+  <title>Merci !!! | Geek & Dragon</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@600&display=swap" rel="stylesheet">
   <style>
