5a5b07789de155db5b03fa8f87e30f955497be48
Brujah <138628049+VraiBrujah@users.noreply.github.com>
2025-08-09 21:21:29 -0400

    Load environment variables via Dotenv

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/bootstrap.php';
 $active = 'index';
 $title  = 'Geek & Dragon | Aides de jeu immersives';
 $metaDescription = "Cartes, pièces et fiches pour enrichir vos parties de D&D.";
