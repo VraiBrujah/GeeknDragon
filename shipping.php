2c625f6a7784f296a2f2666d934a87897f958811
VraiBrujah <mathieubrujah@gmail.com>
2025-08-12 12:38:16 -0400

    Update shipping.php

--- shipping.php
+++ shipping.php
@@ -60,5 +60,6 @@ $rates = [
 
 // Réponse attendue par Snipcart
 http_response_code(200);
-header('Content-Type: application/json');
-echo json_encode($rates);
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode(['rates' => $rates], JSON_UNESCAPED_UNICODE);
+exit;
