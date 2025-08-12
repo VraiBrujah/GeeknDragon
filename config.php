728937d23633fb3314d881005ca4f06dfca24fe8
Brujah <138628049+VraiBrujah@users.noreply.github.com>
2025-08-12 19:20:19 -0400

    Remove getenv fallbacks in config

--- config.php
+++ config.php
@@ -2,30 +2,22 @@
 return [
     'smtp' => [
         'host' => $_ENV['SMTP_HOST']
-            ?? $_SERVER['SMTP_HOST']
-            ?? getenv('SMTP_HOST'),
+            ?? $_SERVER['SMTP_HOST'],
         'username' => $_ENV['SMTP_USERNAME']
-            ?? $_SERVER['SMTP_USERNAME']
-            ?? getenv('SMTP_USERNAME'),
+            ?? $_SERVER['SMTP_USERNAME'],
         'password' => $_ENV['SMTP_PASSWORD']
-            ?? $_SERVER['SMTP_PASSWORD']
-            ?? getenv('SMTP_PASSWORD'),
+            ?? $_SERVER['SMTP_PASSWORD'],
         'port' => (
             $_ENV['SMTP_PORT']
             ?? $_SERVER['SMTP_PORT']
-            ?? getenv('SMTP_PORT')
         ) ?: 587,
     ],
     'recaptcha_site_key' => $_ENV['RECAPTCHA_SITE_KEY']
-        ?? $_SERVER['RECAPTCHA_SITE_KEY']
-        ?? getenv('RECAPTCHA_SITE_KEY'),
+        ?? $_SERVER['RECAPTCHA_SITE_KEY'],
     'recaptcha_secret_key' => $_ENV['RECAPTCHA_SECRET_KEY']
-        ?? $_SERVER['RECAPTCHA_SECRET_KEY']
-        ?? getenv('RECAPTCHA_SECRET_KEY'),
+        ?? $_SERVER['RECAPTCHA_SECRET_KEY'],
     'snipcart_api_key' => $_ENV['SNIPCART_API_KEY']
-        ?? $_SERVER['SNIPCART_API_KEY']
-        ?? getenv('SNIPCART_API_KEY'),
+        ?? $_SERVER['SNIPCART_API_KEY'],
     'snipcart_secret_api_key' => $_ENV['SNIPCART_SECRET_API_KEY']
-        ?? $_SERVER['SNIPCART_SECRET_API_KEY']
-        ?? getenv('SNIPCART_SECRET_API_KEY'),
+        ?? $_SERVER['SNIPCART_SECRET_API_KEY'],
 ];
