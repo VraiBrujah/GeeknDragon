d4f28335d4c190cfafac9f18e822f033b62eec88
VraiBrujah <mathieubrujah@gmail.com>
2025-08-14 20:07:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $metaDescription = $translations['meta']['home']['desc'] ?? '';
   echo $snipcartInit;
   ?>
 
-  <main id="main" class="pt-650">
+  <main id="main" class="pt-[200]">
   
 
     <!-- ===== HERO ===== -->
