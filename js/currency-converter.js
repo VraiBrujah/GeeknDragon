c0b49bc2fa6608c7db15d7ee1155657992886cce
Brujah <138628049+VraiBrujah@users.noreply.github.com>
2025-08-15 21:16:05 -0400

    Clear default zero on focus in currency converter

--- js/currency-converter.js
+++ js/currency-converter.js
@@ -73,6 +73,19 @@
 
   sources.forEach((inputEl) => {
     const el = inputEl;
+    el.addEventListener('focus', () => {
+      if (el.value === '0') {
+        el.value = '';
+      }
+    });
+
+    el.addEventListener('blur', () => {
+      if (el.value === '') {
+        el.value = '0';
+      }
+      render();
+    });
+
     el.addEventListener('input', () => {
       el.value = el.value.replace(/[^0-9]/g, '');
       render();
