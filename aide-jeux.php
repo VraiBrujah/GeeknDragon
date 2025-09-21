489ac7ffed2c1644e6427993fcfc16f35181ac5f
VraiBrujah <mathieubrujah@gmail.com>
2025-09-21 19:39:20 -0400

    Revert "Update aide-jeux.php"
    This reverts commit 19044a54718988a955e312f5a87954cacef804dd.

--- aide-jeux.php
+++ aide-jeux.php
@@ -1044,43 +1044,43 @@ echo $snipcartInit;
           <h3 class="text-2xl font-bold mb-6 text-center text-amber-400">💎 Types de Pièces</h3>
           
           <div class="space-y-4">
-            <div class="flex items-center p-3 bg-gray-800/50 rounded-lg">
-              <div class="w-12 h-12 bg-yellow-600 rounded-full flex items-center justify-center text-white font-bold mr-4">PP</div>
-              <div class="flex-1">
-                <h4 class="font-semibold text-yellow-400 whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Pièce de Platine (pp)</h4>
-                <p class="text-gray-300 text-sm whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;La plus précieuse • 1&nbsp;pp = 10&nbsp;po</p>
+            <div class="flex items-center space-x-4 p-3 bg-gray-800/50 rounded-lg">
+              <div class="w-12 h-12 bg-yellow-600 rounded-full flex items-center justify-center text-white font-bold">PP</div>
+              <div>
+                <h4 class="font-semibold text-yellow-400">Pièce de Platine (pp)</h4>
+                <p class="text-gray-300 text-sm">La plus précieuse • 1 pp = 10 po</p>
               </div>
             </div>
             
-            <div class="flex items-center p-3 bg-gray-800/50 rounded-lg">
-              <div class="w-12 h-12 bg-yellow-500 rounded-full flex items-center justify-center text-black font-bold mr-4">PO</div>
-              <div class="flex-1">
-                <h4 class="font-semibold text-yellow-400 whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Pièce d'Or (po)</h4>
-                <p class="text-gray-300 text-sm whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Monnaie de référence • 1&nbsp;po = 10&nbsp;pa</p>
+            <div class="flex items-center space-x-4 p-3 bg-gray-800/50 rounded-lg">
+              <div class="w-12 h-12 bg-yellow-500 rounded-full flex items-center justify-center text-black font-bold">PO</div>
+              <div>
+                <h4 class="font-semibold text-yellow-400">Pièce d'Or (po)</h4>
+                <p class="text-gray-300 text-sm">Monnaie de référence • 1 po = 10 pa</p>
               </div>
             </div>
             
-            <div class="flex items-center p-3 bg-gray-800/50 rounded-lg">
-              <div class="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center text-black font-bold mr-4">PA</div>
-              <div class="flex-1">
-                <h4 class="font-semibold text-gray-400 whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Pièce d'Argent (pa)</h4>
-                <p class="text-gray-300 text-sm whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Monnaie courante • 1&nbsp;pa = 10&nbsp;pe</p>
+            <div class="flex items-center space-x-4 p-3 bg-gray-800/50 rounded-lg">
+              <div class="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center text-black font-bold">PA</div>
+              <div>
+                <h4 class="font-semibold text-gray-400">Pièce d'Argent (pa)</h4>
+                <p class="text-gray-300 text-sm">Monnaie courante • 1 pa = 10 pe</p>
               </div>
             </div>
             
-            <div class="flex items-center p-3 bg-gray-800/50 rounded-lg">
-              <div class="w-12 h-12 bg-orange-600 rounded-full flex items-center justify-center text-white font-bold mr-4">PE</div>
-              <div class="flex-1">
-                <h4 class="font-semibold text-orange-400 whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Pièce d'Électrum (pe)</h4>
-                <p class="text-gray-300 text-sm whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Alliage or-argent • 1&nbsp;pe = 5&nbsp;pc</p>
+            <div class="flex items-center space-x-4 p-3 bg-gray-800/50 rounded-lg">
+              <div class="w-12 h-12 bg-orange-600 rounded-full flex items-center justify-center text-white font-bold">PE</div>
+              <div>
+                <h4 class="font-semibold text-orange-400">Pièce d'Électrum (pe)</h4>
+                <p class="text-gray-300 text-sm">Alliage or-argent • 1 pe = 5 pc</p>
               </div>
             </div>
             
-            <div class="flex items-center p-3 bg-gray-800/50 rounded-lg">
-              <div class="w-12 h-12 bg-orange-800 rounded-full flex items-center justify-center text-white font-bold mr-4">PC</div>
-              <div class="flex-1">
-                <h4 class="font-semibold text-orange-400 whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Pièce de Cuivre (pc)</h4>
-                <p class="text-gray-300 text-sm whitespace-nowrap">&nbsp;&nbsp;&nbsp;&nbsp;Menue monnaie • La plus commune</p>
+            <div class="flex items-center space-x-4 p-3 bg-gray-800/50 rounded-lg">
+              <div class="w-12 h-12 bg-orange-800 rounded-full flex items-center justify-center text-white font-bold">PC</div>
+              <div>
+                <h4 class="font-semibold text-orange-400">Pièce de Cuivre (pc)</h4>
+                <p class="text-gray-300 text-sm">Menue monnaie • La plus commune</p>
               </div>
             </div>
           </div>
